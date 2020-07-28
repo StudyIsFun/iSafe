6d2a04cd238314831673ca0ac559571b40df40e4
Ritesh Singh <risi1006@gmail.com>
2020-07-28 10:13:30 +0530

    Fixed phone signin

--- app/src/main/java/com/crime_mapping/electrothon/sos/PhoneNumberFragment.java
+++ app/src/main/java/com/crime_mapping/electrothon/sos/PhoneNumberFragment.java
@@ -2,6 +2,7 @@ package com.crime_mapping.electrothon.sos;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +11,14 @@ import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class PhoneNumberFragment extends Fragment {
 
     TextInputEditText textInputEditText;
     TextInputLayout textInputLayout;
-
+    FirebaseUser user;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -28,6 +31,12 @@ public class PhoneNumberFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_phone_number, container, false);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        }
         textInputLayout = view.findViewById(R.id.tilPhoneNumber);
         Button submit = view.findViewById(R.id.btnGetCode);
         submit.setOnClickListener(new View.OnClickListener() {
